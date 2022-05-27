7ad5ff8dded4f5e9fb0861b1206b0ea5bbc7934d
Ömer IŞIK <isikomer373@gmail.com>
2022-05-27 16:37:18 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{flex:1}}>
+    <View style={styles.container}>
+      <Text>Hello</Text>
+      
+    </View>
+    
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
